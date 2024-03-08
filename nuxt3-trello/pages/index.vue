8b24b4e0e7ad1d736ddef588f16bd414b760ea57
Kristina140699 <Kristina.11915169@lpu.in>
2024-03-08 23:55:09 +0530

    Feature to add workspaces

--- nuxt3-trello/pages/index.vue
+++ nuxt3-trello/pages/index.vue
@@ -1,13 +1,45 @@
-<script>
-export default {}
+<script setup>
+import { ref } from 'vue'
+
+const newWorkspaceName = ref('')
+const workspaceList = ref([])
+const createWorkspace = () => {
+    const randomId = Math.floor(Math.random()*100)
+
+    workspaceList.value.push({
+        id: randomId,
+        name: newWorkspaceName.value
+    })
+}
 </script>
 
 <template>
     <h1>Home Page</h1>
     <h2>Reacently Viewed</h2>
     <h2>Workspaces</h2>
-    <p>This is a test</p>
+    <input type = "text" v-model="newWorkspaceName">
+    <button @click="createWorkspace">Create a Workspace</button>
+    <ul class="workspace-list">
+        <li v-for="workspace in workspaceList" :key="workspace.id"
+        class="workspace-card">
+        
+        {{ workspace.id }}: {{ workspace.name }}
+        </li>
+    </ul>
 </template>
 
 <style>
+.workspace-card{
+    background-color: rgb(203, 252, 180);
+    display: block;
+    border: 2px solid green;
+    border-radius: 5px;
+    padding: 2rem;
+    margin-bottom: 1rem;
+}
+
+.workspace-list{
+    margin-left: 0;
+    padding-left: 0;
+}
 </style>
