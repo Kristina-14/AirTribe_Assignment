42988569279f553240c93ef54a21902206ba3090
Kristina140699 <Kristina.11915169@lpu.in>
2024-03-08 20:20:46 +0530

    Initial commit: Added Nuxt.js project

---
+++ nuxt3-trello/pages/index.vue
@@ -0,0 +1,13 @@
+<script>
+export default {}
+</script>
+
+<template>
+    <h1>Home Page</h1>
+    <h2>Reacently Viewed</h2>
+    <h2>Workspaces</h2>
+    <p>This is a test</p>
+</template>
+
+<style>
+</style>
