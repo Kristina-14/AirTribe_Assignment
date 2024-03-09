97f8e243b97c2e2d3d4cc0c9059a7f26688e5afb
Kristina140699 <Kristina.11915169@lpu.in>
2024-03-09 11:40:10 +0530

    Added the feature of Create-Columns

--- nuxt3-trello/pages/workspace/[id].vue
+++ nuxt3-trello/pages/workspace/[id].vue
@@ -8,8 +8,20 @@ export default{
         }
     },
     data: () =>({
-        workspaceName: ''
+        workspaceName: '',
+        board: {
+            name : 'Apples',
+            columns: []
+        }
     }),
+    methods:
+    {
+        createColumn(){
+            this.board.columns.push({
+                items: []
+            })
+        }
+    },
     mounted(){
         this.workspaceName = this.workspaceList.find(
             workspace => workspace.id === Number(this.$route.params.id)
@@ -21,9 +33,31 @@ export default{
 
 <template><div>
 <h1>{{ workspaceName }} WorkSpace (#{{ $route.params.id }})</h1>
-<p>{{ $route.params.id }} </p>
+<section>
+    <h2>{{ board.name }}</h2>
+    <button @click="createColumn">Create Columns </button>
+<div class="column-grid">
+    <ul class="board-column" v-for="column in board.columns"> </ul>
+
+</div>
+
+
+</section>
 </div>
 </template>
 
 <style>
+.column-grid {
+    display: grid;
+    grid-template-columns: repeat(v-bind(board.columns.length),
+     1fr);
+}
+.board-column {
+    background-color: powderblue;
+    border: 1px solid blue;
+    border-radius: 2px;
+    height: 80vh;
+    width: 100px;
+    margin-right: 1rem;
+}
 </style>
